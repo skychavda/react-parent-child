ef5c907b45ed7fcc30bbd9782a2d4c15a4781c84
Akash Chavda <skychavda9@gmail.com>
2019-01-04 09:53:20 +0530

    fix: build

---
+++ precache-manifest.20cb73f2d1a653bdc2e6bee60cf6ceab.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "5f6346c78f2455d50171",
+    "url": "./static/js/main.5f6346c7.chunk.js"
+  },
+  {
+    "revision": "fbfa9ac1c04d6f818c50",
+    "url": "./static/js/1.fbfa9ac1.chunk.js"
+  },
+  {
+    "revision": "b97df55e74220991b8d402202b13e0ba",
+    "url": "./index.html"
+  }
+];
